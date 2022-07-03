45bbbb34427ca0b2231a37733b2f9a0cd2137200
Mariana <mariana.c.novaes22@gmail.com>
2022-07-03 17:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.*/
+
+            for (int i = 1; i < 51; i++)
+            {
+                Console.Write(i + "\n");
+            }
+            Console.ReadKey();  
+        }
+        
+    }
+}
